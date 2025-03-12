7ee388fc50177fe27dad77208ad82142f1d74c58
Karina Savoie <karinasavoie@Karinas-MacBook-Pro.local>
2025-03-12 11:49:23 +0000

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,11 @@ export const metadata = {
   title: "Kalro Education",
   description:
     "Kalro Education is a specialist consultancy focused exclusively on the UK education sector. We partner with Local Authorities, Mainstream and SEND school operators, and nurseries to acquire, develop, and repurpose properties into high-quality learning environments.",
+  icons: {
+    icon: "/k-logo.ico",
+    shortcut: "/k-logo.ico",
+    apple: "/k-logo.png",
+  },
 };
 
 export default function RootLayout({ children }) {
